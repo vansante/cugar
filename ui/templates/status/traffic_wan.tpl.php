ddf8f13c7c909fb91cdd37225cbb3caf1c7764bf
pvansanten <pvansanten@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-09-21 09:02:43 +0000

    Strip more unnecessary pages

--- ui/templates/status/traffic_wan.tpl.php
+++
@@ -1,8 +0,0 @@
-<h2>Traffic on WAN interface</h2>
-
-<p class="intro">You can view the traffic statistics for the WAN interface here.</p>
-
-<?
-$this->status_traffic_id = 'wan';
-include $this->template('status/traffic_table.tpl.php');
-?>
