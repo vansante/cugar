b7e318f1d90396a55dcb6c8ab3c746abf80c7d45
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-09-22 12:43:56 +0000

    Updated createimage script Re-stripped the kernel due to a compile error

--- createimage.sh
+++ createimage.sh
@@ -5,3 +5,6 @@ svn update
 rm -rf /usr/src/tools/tools/nanobsd/Files
 # copy over new files
 cp -r /home/cugar/Files /usr/src/tools/tools/nanobsd/
+
+# create image
+sh /usr/src/tools/tools/nanobsd/nanobsd.sh -c /home/cugar/cugar-nano.conf
