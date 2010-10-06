e67938fee5b90b6da4f3c4db50c9a1a48f0b936f
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-10-06 10:55:53 +0000

    Changed kernel for 7.2 building altered rc.conf for testing purposes
    attempted to fix createpackages script a bit, still horribly broken
    edited packages.list, contained typo
    added skipping buildworld to createimage.sh

--- createimage.sh
+++ createimage.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 cd /home/cugar
 svn update
+# copy over debug config files while we're not parsing configurations
+cp -r /home/cugar/configfiles/* /home/cugar/Files/etc/
 # clean old files from directory (to ensure new files are accurate)
 rm -rf /usr/src/tools/tools/nanobsd/Files
 # copy over new files
 cp -r /home/cugar/Files /usr/src/tools/tools/nanobsd/
 
 # create image
-sh /usr/src/tools/tools/nanobsd/nanobsd.sh -c /home/cugar/cugar-nano.conf
+sh /usr/src/tools/tools/nanobsd/nanobsd.sh -w -c /home/cugar/cugar-nano.conf
