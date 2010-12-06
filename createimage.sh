4634616c4cf0415a4635e8a7fd886dbf064415bc
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-12-06 09:18:26 +0000

    changed some of the scripts around to allow for the new filepath (/Client)

--- createimage.sh
+++ createimage.sh
@@ -4,11 +4,11 @@ setenv CFG_ROOT_PASSWORD toor
 cd /home/cugar
 svn update
 # copy over debug config files while we're not parsing configurations
-cp -r /home/cugar/configfiles/* /home/cugar/Files/etc/
+cp -r /home/cugar/Client/configfiles/* /home/cugar/Client/Files/etc/
 # clean old files from directory (to ensure new files are accurate)
 rm -rf /usr/src/tools/tools/nanobsd/Files
 # copy over new files
-cp -r /home/cugar/Files /usr/src/tools/tools/nanobsd/
+cp -r /home/cugar/Client/Files/usr/src/tools/tools/nanobsd/
 chmod -R +x /usr/src/tools/tools/nanobsd/Files 
 # create image
 sh /usr/src/tools/tools/nanobsd/nanobsd.sh -w -c /home/cugar/cugar-nano.conf
