f1580171477a7a9f526b3428a7ab246794907a33
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-12-13 11:47:20 +0000

    updated the parser with some of the options that get streamed in clientside before parsing (ip addresses and the likes) so we can have an actual functional device if we parse yonder configuration.

--- Client/Files/usr/local/lib/CUGAR/parser/subnet_mask.php
+++ Client/Files/usr/local/lib/CUGAR/parser/subnet_mask.php
@@ -25,7 +25,8 @@
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
  */
-class ip extends Statement{
+class subnet_mask extends Statement{
+
 	/**
 	 * Constructor
 	 *
@@ -38,44 +39,21 @@ class ip extends Statement{
 
 	/**
 	 * (non-PHPdoc)
-	 * @see Files/usr/local/lib/CUGAR/parser/Statement#interpret($options)
+	 * @see Client/Files/usr/local/lib/CUGAR/parser/Statement#interpret($options)
 	 */
 	public function interpret($options){
 		$this->validate($options);
-		
-		if($this->parse_options['conf_block'] == 'dhcp_relay'){
-			$ref = DHCPRelayConfig::getInstance();
-			$ref->addServer((string)$options);
-		}
-		elseif($this->parse_options['conf_block'] == 'hostapd'){
-			$ref = HostAPDConfig::getInstance();
-			if($this->parse_options['radius_server_type'] == 'auth'){
-				$ref->setRadiusAuthIp((string)$options);
-			}
-			else{
-				$ref->setRadiusAcctIp((string)$options);
-			}
-		}
-		elseif($this->parse_options['conf_block'] == 'address'){
-			$ref = System::getInstance();
-			$ref->setAddress((string)$options);
-		}
-		elseif($this->parse_options['conf_block'] == 'dns_servers'){
-			$ref = System::getInstance();
-			$ref->addDNSserver((string)$options);
-		}
+		$ref = System::getInstance();
+		$ref->setSubnetmask((string)$options);
 	}
 
 	/**
 	 * (non-PHPdoc)
-	 * @see Files/usr/local/lib/CUGAR/parser/Statement#validate($options)
+	 * @see Client/Files/usr/local/lib/CUGAR/parser/Statement#validate($options)
 	 */
 	public function validate($options){
 		if(long2ip(ip2long((string)$options)) != (string)$options){
-			// @TODO: Do IPV6 validation (no stock PHP function yet)
-			if(!false){
-				ParseErrorBuffer::addError('invalid IP address',ParseErrorBuffer::$E_FATAL,$options);
-			}
+			ParseErrorBuffer::addError('invalid subnet mask',ParseErrorBuffer::$E_FATAL,$options);
 		}
 	}
 }
