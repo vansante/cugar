b85097199a07313c95e3dedaaf0e69b1bb609a16
pvansanten <pvansanten@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-12-28 15:40:21 +0000

    Check device by checking an encrypted string

--- Server/apps/frontend/modules/xml/actions/actions.class.php
+++ Server/apps/frontend/modules/xml/actions/actions.class.php
@@ -10,20 +10,40 @@
  */
 class xmlActions extends sfActions {
 
-    /**
-     * Executes index action
-     *
-     * @param sfRequest $request A request object
-     */
+
     public function executeGet(sfWebRequest $request) {
 
-        $device = DeviceTable::getFromCertificateName($request->getParameter('cert_name'));
+        $cert_name = $request->getParameter('cert_name');
+        $cert_name_encrypted = $request->getParameter('name_check');
+
+        $this->forward404Unless($this->checkDevice($cert_name, $cert_name_encrypted), 'Name check failed');
+
+        $device = DeviceTable::getFromCertificateName($cert_name);
         
-        $this->forward404Unless($device, 'Device not found');
+        $this->forward404Unless($device, 'Device not found in database');
 
         $xml = new XMLConfig($device);
 
         $this->xml = $xml->getXML();
     }
 
+    protected function checkDevice($cert_name, $cert_name_encrypted) {
+        if (!strlen($cert_name) || !strlen($cert_name_encrypted)) {
+            return false;
+        }
+
+        $cert_dir = sfConfig::get('app_certificate_dir');
+        $file = $cert_dir.DIRECTORY_SEPARATOR.$cert_name.'.crt';
+
+        if (!file_exists($file)) {
+            return false;
+        }
+
+        $key = file_get_contents($file);
+
+        $cert_name_decrypted = false;
+        $result = openssl_public_decrypt($cert_name_enc, $cert_name_decrypted, $key);
+        
+        return $result && $cert_name == $cert_name_decrypted;
+    }
 }
