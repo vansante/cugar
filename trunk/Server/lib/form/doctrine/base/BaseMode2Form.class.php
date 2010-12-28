88e3b2c845e5c0cfe52a6716e868949fc039ee8f
pvansanten <pvansanten@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2010-12-28 13:36:23 +0000

    Only alphanumeric chars in SSID names

--- trunk/Server/lib/form/doctrine/base/BaseMode2Form.class.php
+++ trunk/Server/lib/form/doctrine/base/BaseMode2Form.class.php
@@ -10,22 +10,24 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormGeneratedInheritanceTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
  */
-abstract class BaseMode2Form extends RadiusSsidForm {
+abstract class BaseMode2Form extends RadiusSsidForm
+{
+  protected function setupInheritance()
+  {
+    parent::setupInheritance();
 
-    protected function setupInheritance() {
-        parent::setupInheritance();
+    $this->widgetSchema   ['config_id'] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Config'), 'add_empty' => false));
+    $this->validatorSchema['config_id'] = new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Config')));
 
-        $this->widgetSchema ['config_id'] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Config'), 'add_empty' => false));
-        $this->validatorSchema['config_id'] = new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Config')));
+    $this->widgetSchema   ['file_name'] = new sfWidgetFormInputText();
+    $this->validatorSchema['file_name'] = new sfValidatorString(array('max_length' => 255, 'min_length' => 4));
 
-        $this->widgetSchema ['file_name'] = new sfWidgetFormInputText();
-        $this->validatorSchema['file_name'] = new sfValidatorString(array('max_length' => 255, 'min_length' => 4));
+    $this->widgetSchema->setNameFormat('mode2[%s]');
+  }
 
-        $this->widgetSchema->setNameFormat('mode2[%s]');
-    }
-
-    public function getModelName() {
-        return 'Mode2';
-    }
+  public function getModelName()
+  {
+    return 'Mode2';
+  }
 
 }
