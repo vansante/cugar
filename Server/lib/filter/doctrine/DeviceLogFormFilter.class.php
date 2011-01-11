2699cd08bcea791e473cec7aba94e3625a4b2634
pvansanten <pvansanten@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-01-11 10:25:08 +0000

    Device log display

--- Server/lib/filter/doctrine/DeviceLogFormFilter.class.php
+++ Server/lib/filter/doctrine/DeviceLogFormFilter.class.php
@@ -8,9 +8,10 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class DeviceLogFormFilter extends BaseDeviceLogFormFilter
-{
-  public function configure()
-  {
-  }
+class DeviceLogFormFilter extends BaseDeviceLogFormFilter {
+
+    public function configure() {
+        unset($this['description']);
+    }
+
 }
