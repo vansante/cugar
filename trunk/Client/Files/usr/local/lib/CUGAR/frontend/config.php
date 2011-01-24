c41b8d3e7483ec8d48464fceb949c6f8e617208b
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-01-24 14:14:02 +0000

    Kicked off clientside back-end implementation

---
+++ trunk/Client/Files/usr/local/lib/CUGAR/frontend/config.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ All rights reserved.
+ Copyright (C) 2010-2011 CUGAR
+ All rights reserved.
+
+ All rights reserved.
+ Copyright (C) 2009-2010 GenericProxy <feedback@genericproxy.org>.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Create an object from config.xml
+ *
+ * Original author:
+ * @author Sebastiaan Gibbon
+ * @version 0
+ */
+class Config {
+
+        /**
+         * @var SimpleXMLElement Config in a XML object
+         */
+        public $xml;
+
+        /**
+         * @var string Filename of the configuration file.
+         */
+        public $file;
+
+        /**
+         * Opens a configuration file
+         * 
+         * @param string $file URI to the file to be opened.
+         * @throws Exception on failing to load.
+         */
+        public function __construct($file){
+                $previouslibxmlSetting = libxml_use_internal_errors(true);// Using custom error throwing.
+
+                $this->xml = simplexml_load_file($file);
+                $this->file = $file;
+
+                //Failed loading the XML, throw excption.
+                if (!$this->xml){
+                        $message = "Failed to load configuration file {$file}. Invalid XML. ";
+                        foreach(libxml_get_errors() as $error) {
+                                $message .= $error->message;
+                        }
+                                
+                        libxml_clear_errors();
+                        throw new Exception($message);
+                }
+
+                libxml_use_internal_errors($previouslibxmlSetting);//Set back to default
+        }
+
+        /**
+         * Saves the configuration to file.
+         * 
+         * @param string $file leave empty to use original file or specify another file.
+         * @return bool Returns TRUE on success.
+         */
+        public function saveConfig($file = null) {
+                shell_exec('mount /cfg');
+                $file = is_null($file) ? $this->file : $file;
+                $return = $this->xml->asXML($file);
+                if(!is_dir('/cfg/CUGAR')){
+                       shell_exec('mkdir /cfg/CUGAR');
+                }
+                shell_exec('cp -r /etc/CUGAR/* /cfg/CUGAR/');
+              	shell_exec('umount /cfg');
+                return $return;
+        }
+
+        /**
+         * Get a specific tag, found in the root of the config file.
+         * 
+         * If the element does not exist, one will be created.
+         * If name is null, or not given, the root XML tag will be returned.  
+         * 
+         * @param string $name Name of the XML tag.
+         * @return SimpleXMLElement
+         */
+        public function getElement($name){
+                if (empty($name)){
+                        return $this->xml;
+                } elseif ($this->xml->{$name}[0]){
+                        return $this->xml->{$name}[0];
+                } else {
+                        return $this->xml->addChild($name);
+                }
+        }
+
+        /**
+         * Delete a specific tag found in the root or remove the SimpleXMLElement node from the config.
+         * 
+         * @param string|SimpleXMLElement $node Node of the XML tag to be removed.
+         * @return void
+         */
+        public function deleteElement($node){
+                if (is_string($node)){
+                        unset($this->xml->{$node}[0]);
+                } elseif ($node instanceof SimpleXMLElement){
+                        $dom=dom_import_simplexml($node);
+                        $dom->parentNode->removeChild($dom);
+                }
+        }
+}
+?>
---
+++ trunk/Client/Files/usr/local/lib/CUGAR/frontend/settings.inc.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ All rights reserved.
+ Copyright (C) 2010-2011 CUGAR
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ */
+class settings{
+	private $config;
+	
+	/**
+	 * 
+	 * 
+	 * @param Config $config
+	 */
+	public function __construct($config){
+		$this->config = $config;
+	}
+	
+	/**
+	 * Handle request made by AJAX frontend
+	 */
+	public function parse(){
+		if($_POST['page'] == 'getconfig'){
+			$this->getconfig();
+		}
+		if($_POST['page'] == 'save'){
+			$this->saveconfig();
+		}
+	}
+	
+	/**
+	 * save updated configuration received from AJAX frontend
+	 */
+	private function saveconfig(){
+		$current = $this->config->getElement('hardware');
+		$current->hostname = $_POST['basic_settings_hostname'];
+		$current->mode = $_POST['basic_settings_wl_mode'];
+		$current->channel = $_POST['basic_settings_wl_channel'];
+		
+		/*	Delete Address node and children because of the differences in child nodes for
+		 *	different settings 
+		 */
+		$this->config->deleteElement($current->address);
+		$address = $current->addChild('address');
+		$address->addAttribute('type',$_POST['basic_settings_type']);
+		if($_POST['basic_settings_type'] == 'static'){
+			$address->addChild('ip',$_POST['basic_settings_static_ipaddr']);
+			$address->addChild('subnet_mask',$_POST['basic_settings_static_subnet_mask']);
+			$address->addChild('default_gateway',$_POST['basic_settings_static_default_gateway']);
+			
+			$dns = $address->addChild('dns_servers');
+			$dns->addChild('ip',$_POST['basic_settings_static_dns_server_1']);
+			if(!empty($_POST['basic_settings_static_dns_server_2'])){
+				$dns->addChild('ip',$_POST['basic_settings_static_dns_server_2']);
+			}
+		}
+		
+		$this->config->saveConfig();
+	}
+	
+	/**
+	 * return basic settings from the current sysconf.xml
+	 */
+	private function getconfig(){
+		$current = $this->config->getElement('hardware');
+		echo '<reply action="ok"><basic_settings>';
+		echo '<hostname>'.(string)$current->hostname.'</hostname>';
+		echo '<type>'.(string)$current->address['type'].'</type>';
+		if($current->address['type'] == 'static'){
+			echo '<static>';
+			echo '<ipaddr>'.(string)$current->address->ip.'</ipaddr>';
+			echo '<subnetmask>'.(string)$current->address->subnetmask.'</subnmetmask>';
+			echo '<default_gateway>'.$current->address->default_gateway.'</default_gateway>';
+			echo '<dns_server1>'.(string)$current->address->dns_servers->ip[0].'</dns_server1>';
+			if(isset($current->address->dns_servers->ip[1])){
+				echo '<dns_server2>'.(string)$current->address->dns_servers->ip[1].'</dns_server2>';
+			}
+			echo '</static>';
+		}
+		echo '<hardware>';
+		echo '<mode>'.(string)$current->mode.'</mode>';
+		echo '<channel>'.(string)$current->channel.'</channel>';
+		echo '</hardware>';
+		echo '</basic_settings></reply>';
+	}
+}
