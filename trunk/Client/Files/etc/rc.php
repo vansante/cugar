0cfafd925714182a736c7c7b55b5bbe6228579b1
sleebet@gmail.com <sleebet@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-01-25 10:22:10 +0000

    rc + rc.php inial version

---
+++ trunk/Client/Files/etc/rc.php
@@ -0,0 +1,31 @@
+#!/usr/local/bin/php
+<?php 
+include_once('/usr/local/lib/CUGAR/bootstrap/bootstrap.php');
+require_once('/usr/local/lib/CUGAR/comm/Comm.php');
+require_once('/usr/local/lib/CUGAR/comm/FetchConfig.php');
+error_reporting(0);
+$strap = new BootStrap();
+
+require_once('/usr/local/lib/CUGAR/parser/Parser.php');
+require_once('/usr/local/lib/CUGAR/parser/Statement.php');
+require_once('/usr/local/lib/CUGAR/parser/config.php');
+require_once('/usr/local/lib/CUGAR/lib/ErrorStore.php');
+require_once('/usr/local/lib/CUGAR/exceptions.php');
+require_once('/usr/local/lib/CUGAR/config/ConfigGenerator.php');
+require_once('/usr/local/lib/CUGAR/config/DHCPRelay.php');
+require_once('/usr/local/lib/CUGAR/config/Hostap.php');
+require_once('/usr/local/lib/CUGAR/config/OpenVPN.php');
+require_once('/usr/local/lib/CUGAR/config/Portal.php');
+require_once('/usr/local/lib/CUGAR/config/RC.php');
+require_once('/usr/local/lib/CUGAR/config/System.php');
+
+try{
+    $xml = new XMLParser();
+    $xml->setErrorLevel(2);
+    $xml->loadXML('./config.xml');
+    $xml->parse();
+}
+catch(Exception $e){
+    print_r($e);
+}
+?>
