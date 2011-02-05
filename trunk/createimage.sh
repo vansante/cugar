3e71d263a34a0c8bc08a03dad14c1b693ae85d18
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-02-05 07:58:43 +0000

    updated generation scripts and cugar-nano configurations

--- trunk/createimage.sh
+++ trunk/createimage.sh
@@ -8,7 +8,7 @@ cp -r /home/cugar/Client/configfiles/* /home/cugar/Client/Files/etc/
 # clean old files from directory (to ensure new files are accurate)
 rm -rf /usr/src/tools/tools/nanobsd/Files
 # copy over new files
-cp -r /home/cugar/Client/Files/usr/src/tools/tools/nanobsd/
+cp -r /home/cugar/Client/Files /usr/src/tools/tools/nanobsd/
 chmod -R +x /usr/src/tools/tools/nanobsd/Files 
 # create image
-sh /usr/src/tools/tools/nanobsd/nanobsd.sh -w -c /home/cugar/cugar-nano.conf
+sh /usr/src/tools/tools/nanobsd/nanobsd.sh -b -c /home/cugar/cugar-nano.conf
