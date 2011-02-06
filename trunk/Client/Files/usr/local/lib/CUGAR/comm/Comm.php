ed17b21c74c4719006e6abbbcd684d2e4b12cff7
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-02-06 08:29:03 +0000

    re-asserting a requirement for inclusion of ErrorStore functionality in Comm class. The file should have been included at rc.php but for some reason still triggers an error.

--- trunk/Client/Files/usr/local/lib/CUGAR/comm/Comm.php
+++ trunk/Client/Files/usr/local/lib/CUGAR/comm/Comm.php
@@ -25,6 +25,8 @@
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
  */
+require_once('/usr/local/lib/CUGAR/lib/ErrorStore.php');
+
 class Comm{
 	/**
 	 * Directory where the private key resides
