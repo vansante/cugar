3e0c3f671f22215c40f8c8fcccd94249949a9bcb
pvansanten <pvansanten@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-02-08 15:50:28 +0000

    Server settings, work on certif task

--- Server/config/ProjectConfiguration.class.php
+++ Server/config/ProjectConfiguration.class.php
@@ -10,6 +10,7 @@ class ProjectConfiguration extends sfProjectConfiguration {
             'sfDoctrinePlugin',
             'sfDoctrineGuardPlugin',
             'sfJqueryReloadedPlugin',
+            'csSettingsPlugin',
             'sfAdminDashPlugin'
         ));
     }
