7040db0b4ae924200bdbb4a458e8c5b4d0656dd7
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-02-16 10:05:22 +0000

    Refactored Runmode into Functions class for availability in places other than bootstrap.

--- trunk/Client/Files/usr/local/lib/CUGAR/bootstrap/Networking.php
+++ trunk/Client/Files/usr/local/lib/CUGAR/bootstrap/Networking.php
@@ -35,25 +35,16 @@
  */
 class Networking{
 	/**
-	 * Run mode, enables or disables DEBUG flags
-	 * @var Integer
-	 */
-	private $runmode;
-
-	/**
 	 * Configuration block for interface
 	 * @var SimpleXMLElement
 	 */
 	private $config;
 
 	/**
-	 *
-	 * @param Integer $runmode
-	 * @return unknown_type
+	 * @return void
 	 */
-	public function __construct($runmode){
+	public function __construct(){
 		echo "preparing network service \n";
-		$this->runmode = $runmode;
 	}
 
 	/**
