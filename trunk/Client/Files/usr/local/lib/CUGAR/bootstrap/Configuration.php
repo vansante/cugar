b2fbbaab639c4896f5fe0352566a10c9501593a8
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-02-22 08:48:41 +0000

    More bootstrap refactoring, should be finished now. All bootstrap subtasks are now in their own class. Code should be much easier to maintain now.

--- trunk/Client/Files/usr/local/lib/CUGAR/bootstrap/Configuration.php
+++ trunk/Client/Files/usr/local/lib/CUGAR/bootstrap/Configuration.php
@@ -25,11 +25,128 @@
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
  */
+
+abstract class configManager{
+	/**
+	 * Local configuration from the device
+	 * @var SimpleXMLElement
+	 */
+	private $local_conf;
+
+	/**
+	 * Configuration fetched from the server
+	 * @var SimpleXMLElement
+	 */
+	private $server_conf;
+
+	/**
+	 * Check configuration for changes
+	 *
+	 * Check if the configuration we are about to make is the same
+	 * as the one we made previously. If so, abort and continue the boot process as normal.
+	 * @return unknown_type
+	 */
+	abstract function checkConfigVersion();
+
+	/**
+	 * Setter for $this->local_conf
+	 *
+	 * @param SimpleXMLElement $local_conf
+	 * @return void
+	 */
+	public function setLocalConf($local_conf){
+		$this->local_conf = $local_conf;
+	}
+
+	/**
+	 * Setter for $this->server_conf
+	 *
+	 * @param SimpleXMLElement $server_conf
+	 * @return void
+	 */
+	public function setForeignConf($server_conf){
+		$this->server_conf = $server_conf;
+	}
+
+	/**
+	 *
+	 *
+	 * @throws SystemException
+	 * @return unknown_type
+	 */
+	public function writeConfiguration(){
+		echo "Writing configuration to file\n";
+		$fp = fopen('/etc/CUGAR/config.xml',w);
+
+		if($fp){
+			fwrite($fp,$this->server_conf->asXML());
+			fclose($fp);
+
+			//	Copy the config xml to persistent storage on the /cfg slice of the device
+			Functions::shellCommand('cp /etc/CUGAR/config.xml /cfg/CUGAR/config.xml');
+		}
+		else{
+			throw new SystemError(ErrorStore::$E_FATAL,'Could not open config file for writing','502');
+		}
+	}
+}
+
 /**
  * Merge clientside configuration with Server-side configuration
  */
-class MergeConfiguration{
-	
+class MergeConfiguration extends configManager{
+
+	public function __construct(){
+
+	}
+
+	/**
+	 * Merge local configuration with the server imposed configuration
+	 *
+	 * @return unknown_type
+	 */
+	public function mergeConfiguration(){
+		echo "Merging local and foreign configuration \n";
+		$this->server_conf->hardware->addChild('hostname',$this->config->hardware->hostname);
+		$address = $this->server_conf->hardware->addChild('address');
+		$address->addAttribute('type',$this->config->hardware->address['type']);
+		$address->addChild('subnet_mask',$this->config->hardware->address->subnet_mask);
+		$address->addChild('ip',$this->config->hardware->address->ip);
+		$address->addChild('default_gateway',$this->config->hardware->address->default_gateway);
+		$dns = $address->addChild('dns_servers');
+
+		foreach($this->config->hardware->address->dns_servers->ip as $ip){
+			$dns->addChild('ip',(string)$ip);
+		}
+
+		if(isset($this->config->modes->mode1)){
+			$tag = $this->server_conf->addChild('ssid');
+			$tag->addAttribute('mode','1');
+			$hostapd = $tag->addChild('hostapd');
+
+			$hostapd->addChild('ssid_name',$this->local_conf->modes->mode1->ssid_name);
+			$hostapd->addChild('broadcast','true');
+
+			$wpa = $hostapd->addChild('wpa');
+			$wpa->addAttribute('mode',$this->local_conf->modes->mode1->wpa['mode']);
+			$wpa->addChild('passphrase',$this->local_conf->modes->mode1->wpa->passphrase);
+			$wpa->addChild('strict_rekey','true');
+			$wpa->addChild('group_rekey_interval','800');
+		}
+		if(isset($this->local_conf->modes->mode2)){
+			$tag = $this->server_conf->addChild('ssid');
+			$tag->addAttribute('mode','2');
+			$hostapd = $tag->addChild('hostapd');
+
+			$hostapd->addChild('ssid_name',$this->local_conf->modes->mode1->ssid_name);
+			$hostapd->addChild('broadcast','true');
+		}
+	}
+
+	private function checkConfigVersion(){
+
+	}
+
 }
 
 /**
@@ -37,6 +154,15 @@ class MergeConfiguration{
  * Differences in syntax require this to happen
  *
  */
-class GenerateConfiguration{
-	
+class GenerateConfiguration extends ConfigManager{
+
+	public function __construct(){}
+
+	public function generateConfiguration(){
+
+	}
+
+	private function checkConfigVersion(){
+
+	}
 }
