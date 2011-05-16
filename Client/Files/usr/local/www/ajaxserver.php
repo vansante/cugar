ea71db9bb63fdf800fa6438bb3bbeb62748c93c0
rickwoelders@gmail.com <rickwoelders@gmail.com@8a6a809d-44d2-5126-26ec-b632a0212ed1>
2011-05-16 09:55:23 +0000

    fixing crap

--- Client/Files/usr/local/www/ajaxserver.php
+++ Client/Files/usr/local/www/ajaxserver.php
@@ -57,7 +57,7 @@ class Server{
 		switch($_POST['module']){
 			case 'settings':
 				include($this->lib_path.'settings.inc.php');
-				new Settings($config);
+				new settings($config);
 				break;
 			case 'mode':
 				include($this->lib_path.'mode.inc.php');
